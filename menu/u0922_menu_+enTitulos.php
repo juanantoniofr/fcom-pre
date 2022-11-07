d7e095c98eb255848d89c0aab650fbda57cf5ed2
Juan Antonio Fernández Ruiz <juanantonio.fr@gmail.com>
2022-11-07 12:09:59 +0100

    Menu_+En

---
+++ menu/u0922_menu_+enTitulos.php
@@ -0,0 +1,126 @@
+{% set url = url('<current>')|render|render %}
+<div class="mas_en">
+    <h3>+ Títulos</h3>
+    <div class="container-mas-en">
+        
+            <!-- 1.Grados -->
+            <div class="opcion_menu {% if url ends with 'grados' %} display_none {% endif %}">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Grados</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+            
+            <!-- 2.Periodismo -->
+            <div class="opcion_menu {% if url ends with 'periodismo' %} display_none {% endif %}">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Grado Periodismo</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+            
+            <!-- 4.Publicidad y RRPP -->
+            <div class="opcion_menu {% if url ends with 'publicidad-y-relaciones-publicas' %} display_none {% endif %}">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Grados Públicidad y RR-PP</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+
+            <!-- 3.CAV -->doblegrado-PER-CAV
+            <div class="opcion_menu {% if url ends with 'comunicacion-audiovisual' %} display_none {% endif %}">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Grado Comunicación Audiovisual</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+
+            <!-- 5.Doble Grado -->
+            <div class="opcion_menu">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Doble Grado</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+
+            <!-- 6.doctorado -->
+            <div class="opcion_menu">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Grados, másteres y doctorado</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+
+            <!-- 7.másteres -->
+            <div class="opcion_menu">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Másteres</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+
+            <!-- 8.Escritura Creativa -->
+            <div class="opcion_menu">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Máster Escritura Creativa</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+
+            <!-- 9.Comunicación y Cultura -->
+            <div class="opcion_menu">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Máster Comunicación y Cultura</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+            
+            <!-- 10.Guión, Narrativa y ... -->
+            <div class="opcion_menu">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Máster Guion, Narrativa y Creatividad Audiovisual</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+
+            <!-- 11.Comunicación institucional .... -->
+            <div class="opcion_menu">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Máster Comunicación Institucional y Política</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+
+            <!-- 12.Publicidad y Gestión... -->
+            <div class="opcion_menu">
+                <img src="/sites/fcom/files/menu-ppal/titulos.png" />
+
+                <div class="descripcion">
+                    <h4>Máster Publicidad y Gestión de Contenidos</h4>
+                    <p>Lorem ipsum.....</p>
+                </div>
+            </div>
+    </div>
+</div>
